1d3d098f870e777fe1a345d1707d8718d2a188f5
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-02-01 05:06:24 +0000

    Bake navmesh on runtime

---
+++ Assets/NavBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+using TrinityGen;
+
+public class NavBuilder : MonoBehaviour
+{
+    public void BuildNavMesh(ArenaPiece[] pieces)
+    {
+        GameObject topPiece = pieces[0].gameObject;
+        NavMeshSurface nav = topPiece.AddComponent<NavMeshSurface>();
+        nav.BuildNavMesh();
+    }
+}
