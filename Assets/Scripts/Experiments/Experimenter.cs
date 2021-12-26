3866c74dc5296a02a9804a082368a24631a86fb7
Nuno Fachada <nuno.fachada@ulusofona.pt>
2021-12-26 19:15:17 +0000

    Experimenter defines params and creates map

--- Assets/Scripts/Experiments/Experimenter.cs
+++ Assets/Scripts/Experiments/Experimenter.cs
@@ -24,37 +24,27 @@ using SnapMeshPCG.SelectionMethods;
 
 namespace SnapMeshPCG.Experiments
 {
+
     public class Experimenter : MonoBehaviour
     {
-
         private IDictionary<string, IDictionary<string, object>> _experiments =
             new Dictionary<string, IDictionary<string, object>>()
             {
                 ["(a)"] = new Dictionary<string, object>()
                 {
                     ["_useSeed"] = true,
-                    ["_seed"] = -2095385667,
+                    ["_seed"] = -267402550,
                     ["_pieceDistance"] = 0.0001f,
                     ["_maxFailures"] = (uint)10,
                     ["_checkOverlaps"] = true,
                     ["_matchingRules"] = SnapRules.Colours | SnapRules.Pins,
                     ["_pinCountTolerance"] = (uint)0,
                     ["_starterConTol"] = (uint)0,
-                    ["_selectionMethod"] = nameof(ArenaSMConfig),
+                    ["_selectionMethod"] = typeof(ArenaSMConfig),
                     ["_selectionParams"] = new Dictionary<string, object>()
                     {
-                        ["_maxPieces"] = 12
+                        ["_maxPieces"] = (uint)12
                     },
-                //     [""] = ,
-                //     [""] = ,
-                //     [""] = ,
-                //     [""] = ,
-                //     [""] = ,
-                //     [""] = ,
-
-
-
-
                 },
                 ["(b)"] = null,
                 ["(c)"] = null,
@@ -72,24 +62,67 @@ namespace SnapMeshPCG.Experiments
         [Button("test", enabledMode: EButtonEnableMode.Editor)]
         private void SetupA()
         {
-            GenerationManager gmInstance = GetComponentInParent<GenerationManager>();
+
+            GenerationManager gmInstance = FindObjectOfType<GenerationManager>();
 
             Type gmType = typeof(GenerationManager);
 
+            Type smType = null;
+            IDictionary<string, object> smParams = null;
+
             foreach (KeyValuePair<string, object> settings in _experiments["(a)"])
             {
-                if (settings.Key.Equals("_selectionParams") || settings.Key.Equals("_selectionMethod"))
+                if (settings.Key.Equals("_selectionMethod"))
                 {
 
-                    continue;
+                    smType = settings.Value as Type;
                 }
-                FieldInfo field = gmType.GetField(settings.Key, BindingFlags.NonPublic | BindingFlags.Instance);
-                field.SetValue(gmInstance, settings.Value);
+                else if (settings.Key.Equals("_selectionParams"))
+                {
+                    smParams = settings.Value as IDictionary<string, object>;
+                }
+                else
+                {
+                    FieldInfo gmField = gmType.GetField(
+                        settings.Key,
+                        BindingFlags.NonPublic | BindingFlags.Instance);
+                    gmField.SetValue(gmInstance, settings.Value);
+                }
             }
-            // foreach (string s in _experiments.Keys)
-            // {
-            //     Debug.Log(s);
-            // }
+
+            if (smType != null)
+            {
+                AbstractSMConfig smCfgInstance =
+                    AbstractSMConfig.GetInstance(smType);
+
+                FieldInfo gmFieldSmName = gmType.GetField(
+                    "_selectionMethod",
+                    BindingFlags.NonPublic | BindingFlags.Instance);
+                gmFieldSmName.SetValue(
+                    gmInstance,
+                    SMManager.Instance.GetNameFromType(smType));
+
+                FieldInfo gmFieldSmParams = gmType.GetField(
+                    "_selectionParams",
+                    BindingFlags.NonPublic | BindingFlags.Instance);
+                gmFieldSmParams.SetValue(gmInstance, smCfgInstance);
+
+                if (smParams != null)
+                {
+                    foreach (KeyValuePair<string, object> smSettings in smParams)
+                    {
+                        FieldInfo smField = smType.GetField(
+                            smSettings.Key,
+                            BindingFlags.NonPublic | BindingFlags.Instance);
+                        smField.SetValue(smCfgInstance, smSettings.Value);
+
+                    }
+                }
+            }
+
+            MethodInfo genMeth = gmType.GetMethod("GenerateMap", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            genMeth.Invoke(gmInstance, null);
 
         }
 
